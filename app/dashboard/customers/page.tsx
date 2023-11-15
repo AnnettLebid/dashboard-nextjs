56cbf7416dea79cf1b942591e5f543240fa53c07
Anna Lebid <annalez16@gmail.com>
2023-11-15 14:56:32 +0200

    add table to customers page

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,16 @@
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+import React from "react";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const customers = await fetchFilteredCustomers(query);
+  return <CustomersTable customers={customers} />;
+}
