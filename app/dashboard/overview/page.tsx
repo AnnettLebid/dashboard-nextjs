df0fee5bcb448d2c79f25c280b21478d88f8c941
Anna Lebid <annalez16@gmail.com>
2023-11-17 17:34:26 +0200

    add Revenue chart

--- app/dashboard/overview/page.tsx
+++ app/dashboard/overview/page.tsx
@@ -2,7 +2,11 @@ import CardWrapper from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import { fetchLatestInvoices, fetchCardData } from "@/app/lib/data";
+import {
+  fetchLatestInvoices,
+  fetchCardData,
+  fetchRevenue,
+} from "@/app/lib/data";
 import { Suspense } from "react";
 import {
   CardsSkeleton,
@@ -11,6 +15,7 @@ import {
 } from "@/app/ui/skeletons";
 
 export default async function Page() {
+  const data = await fetchRevenue();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -18,17 +23,14 @@ export default async function Page() {
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton />}>
-          {/* @ts-expect-error Async Server Component */}
           <CardWrapper />
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
-          {/* @ts-expect-error Async Server Component */}
-          <RevenueChart />
+          <RevenueChart revenues={data} />
         </Suspense>
         <Suspense fallback={<LatestInvoicesSkeleton />}>
-          {/* @ts-expect-error Async Server Component */}
           <LatestInvoices />
         </Suspense>
       </div>
